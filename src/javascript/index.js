5f1e5b359d7d8353a460cc5798afab51bb49034c
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-05-30 23:39:50 -0500

    Removed useless scripts

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,23 +1,8 @@
 
-// window.addEventListener('load', (event) => {
-//    alert('DOM fully loaded and parsed');
-// //    run();
-// });
+let t = document.getElementById("mainTable");
 
+if (t !== null)
+    alert(true);
+else
+    alert(false);
 
-// function run() {
-//     // alert (1);
-
-//     let t = document.getElementById("mainTable");
-
-//     if (t !== null)
-//         alert(true);
-//     else
-//         alert(false);
-
-//     // tbody.innerHTML("<tr>< td > BAHAHAH</td > <td>BAHHA</td><td>$999</td><td>$990</td></tr >");
-
-//     // console.log(tbody.tBodies.length);
-
-// }
-// alert('running index.js');
