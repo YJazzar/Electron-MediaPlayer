71d196dfd3298cff7bcfe5a05a84d366d01c12ba
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-06-06 22:22:16 -0500

    Fixed minor bug to let js files from within html scripts to log to console

--- src/com/tinyMnt/main/javascript/htmlScripts/index.js
+++ src/com/tinyMnt/main/javascript/htmlScripts/index.js
@@ -1,4 +1,9 @@
 
 // Set up the logging elements
 const Logger = require('D:/Projects/Electron-MediaPlayer/src/com/tinyMnt/main/javascript/htmlScripts/htmlLoggerWrapper.js');
-const filename = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\index.js";
+const indexjsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\index.js";
+
+// These values will be available to each of the corresponding files paths due to jquery. They will be used for logging.
+const mainPaneljsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\mainPanel.js";
+const navigationjsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\navigation.js";
+const playerControlsjsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\playerControls.js";
--- src/com/tinyMnt/main/javascript/htmlScripts/mainPanel.js
+++ src/com/tinyMnt/main/javascript/htmlScripts/mainPanel.js
@@ -1,9 +1,7 @@
 const { ipcRenderer } = require("electron");
 const tableBody = document.getElementById("tableBody");
 
-// Set up the logging elements
-const Logger = require('D:/Projects/Electron-MediaPlayer/src/com/tinyMnt/main/javascript/htmlScripts/htmlLoggerWrapper.js');
-const filename = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\mainPanel.js";
+// NOTE: All logging functions required, and the Logger.js object required is inherited from index.js
 
 // "cols" contains the text of each column in the row
 function createRow(cols, path) {
