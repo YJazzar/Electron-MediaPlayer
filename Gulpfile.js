05561525795ff7c4e94a8a804a48a3e404d651a0
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-06-08 21:27:05 -0500

    Create file structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,18 +3,20 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const cleanCSS = require('gulp-clean-css');
 
+const buildPath = "build/";
+
 
 // Copy the index.html as is
 function html() {
     return gulp.src('src/index.html')
-        .pipe(gulp.dest('app/'));
+        .pipe(gulp.dest(buildPath));
 }
 
 // Compile CSS file and move them to the app folder
 function css() {
     return gulp.src('src/**/*.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('app/'));
+        .pipe(gulp.dest(buildPath));
 }
 
 
@@ -22,7 +24,7 @@ function css() {
 function js() { 
     return gulp.src('src/**/*.js')
         .pipe(babel())
-        .pipe(gulp.dest('app/'));
+        .pipe(gulp.dest(buildPath));
 }
 
 
