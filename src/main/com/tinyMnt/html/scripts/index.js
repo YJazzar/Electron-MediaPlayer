e9eaae10418bad52ffbd28fc69a27b892ea48512
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-06-09 21:14:27 -0500

    First version with working react

--- src/main/com/tinyMnt/html/scripts/index.js
+++ src/main/com/tinyMnt/html/scripts/index.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// Set up the logging elements
+console.log("Creating website with react!!");
 
-// const config = require("D:/Projects/Electron-MediaPlayer/config.js");
-// const Logger = require(config.buildPath + config.htmlSourcePath + 'scripts/htmlLoggerWrapper.js');
-// const indexjsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\index.js";
+class App extends React.Component {
+    render() {
+        return <div><h2>Hello Electrate</h2></div>;
+    }
+}
 
-// // These values will be available to each of the corresponding files paths due to jquery. They will be used for logging.
-// const mainPaneljsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\mainPanel.js";
-// const navigationjsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\navigation.js";
-// const playerControlsjsPath = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\playerControls.js";
+window.onload = () => {
+    ReactDOM.render(<App />, document.getElementById('app-load-point'));
+};
--- src/main/com/tinyMnt/html/scripts/navigation.js
+++
@@ -1,6 +0,0 @@
-
-// Set up the logging elements
-// const Logger = require('D:/Projects/Electron-MediaPlayer/src/com/tinyMnt/main/javascript/htmlScripts/htmlLoggerWrapper.js');
-// const filename = "D:\\Projects\\Electron-MediaPlayer\\src\\com\\tinyMnt\\main\\javascript\\htmlScripts\\index.js";
-
-// Logger.logError(filename, "Logging from here");
