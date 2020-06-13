4ab5a972558ee021cdff4ab54573ce49464b2f78
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-06-12 19:38:45 -0500

    Finished renaming the project

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 config = {
-    buildPath: "D:/Projects/Electron-MediaPlayer/build/",
+    buildPath: "D:/Projects/tinyPlayer/build/",
     sourcePath: "main/com/tinyMnt/",
     cssSourcePath: "main/com/tinyMnt/css/", 
     htmlSourcePath: "main/com/tinyMnt/html/", 
     jsSourcePath: "main/com/tinyMnt/jsScripts/",
     reactSourcePath: "main/com/tinyMnt/reactScripts/",
-    loggerPath: "D:/Projects/Electron-MediaPlayer/build/" + "main/com/tinyMnt/jsScripts/" + 'logger/Logger.js',
-    htmlLoggerPath: "D:/Projects/Electron-MediaPlayer/src/main/com/tinyMnt/jsScripts/logger/htmlLogWrapper.js",
+    loggerPath: "D:/Projects/tinyPlayer/build/main/com/tinyMnt/jsScripts/logger/Logger.js",
+    htmlLoggerPath: "D:/Projects/tinyPlayer/src/main/com/tinyMnt/jsScripts/logger/htmlLogWrapper.js",
     defaultLoggingLevel: "debug",
 
     entryHTML: "app.html",
