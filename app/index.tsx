be971079eccff95476e0ac5e36023fb30e2e2da7
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-07-11 22:19:59 -0500

    MAJOR | Switching code to use electron-react-boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './app.global.css';
+
+document.addEventListener('DOMContentLoaded', () =>
+  render(
+    <h1>DOES THIS WORK???? PLEASE TELL ME IT DOES</h1>,
+    document.getElementById('root')
+  )
+);
