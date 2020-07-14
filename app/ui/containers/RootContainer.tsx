e94151e2bb2f0fa00a4c4bef94c32d0c6c61b61e
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-07-14 13:40:54 -0500

    Trying to add containerSize objects to RootContainer

--- app/ui/containers/RootContainer.tsx
+++ app/ui/containers/RootContainer.tsx
@@ -5,6 +5,7 @@ import NavConfig from '../configs/impl/NavConfigImpl';
 import playerConfig from '../configs/impl/PlayerControlsConfigImpl';
 import { ContainerSize } from '../configs/PanelConfig';
 import ResizableContainer from './ResizableContainer';
+import { DefaultDeserializer } from 'v8';
 
 const log = LoggerFactory.getUILogger(__filename);
 
@@ -32,19 +33,56 @@ export default class RootContainer extends React.Component<{}, ContainerSizes> {
     // Called by child components every time it mounts
     initSize(initSize: ContainerSize) {
         log.info(
-            'A new resizable container has just called RootContainer.initSize()'
-        );
-        log.info(
-            `Pushing to state: [${initSize.panelType}] width:[${initSize.width}] height:[${initSize.height}]`
+            `Pushing to RootContainer's state: [${initSize.panelType}] width:[${initSize.width}] height:[${initSize.height}]`
         );
 
+        this.setState(
+            () => {
+                const currState = this.state;
+                if (currState.containerSizes) {
+                    // Find out if the currentState contains initSize that was passed in:
+                    const existsInState = currState.containerSizes.find(
+                        (containerSize: ContainerSize) =>
+                            initSize.panelType === containerSize.panelType
+                    );
+
+                    if (existsInState) {
+                        const newState = currState.containerSizes.map(
+                            (containerSize: ContainerSize, index: number) => {
+                                if (
+                                    initSize.panelType ===
+                                    containerSize.panelType
+                                ) {
+                                    return initSize;
+                                } else {
+                                    return containerSize;
+                                }
+                            }
+                        ) as ContainerSizes;
+                        return { ...newState };
+                    } else {
+                        const arr = currState.containerSizes.concat(initSize);
+                        return { containerSizes: arr } as ContainerSizes;
+                    }
+                } else {
+                    return { containerSizes: [] } as ContainerSizes;
+                }
+            },
+            () => this.printSize.bind(this)
+
+        );
+    }
+
+    printSize() {
+        log.info(
+            `new state size = ${this.state.containerSizes?.length}`
+        )
     }
 
     // Called by child components every time it is resized
     onResize(newSize: ContainerSize) {
         // console.log(`[${newSize.panelType}] [${newSize.width}] [${newSize.height}]`);
     }
-
 
     render() {
         log.info('Creating the rootContainer');
