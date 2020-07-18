f4d79efd5283de5ed2c9c1c09f1f9716fae89896
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-07-17 20:38:25 -0500

    Added POC of how to make a scrollable div

--- app/ui/components/NavigationComponent.tsx
+++ app/ui/components/NavigationComponent.tsx
@@ -3,9 +3,25 @@ import React from 'react';
 export default class NavigationComponent extends React.Component<{}, {}> {
     render() {
         return (
-            <div>
+            <div className="scroll">
                 <h1>Navigation Panel</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien eget mi proin sed libero enim sed faucibus. Nisi est sit amet facilisis magna etiam tempor orci eu. Pharetra convallis posuere morbi leo urna. Id nibh tortor id aliquet lectus. Turpis egestas pretium aenean pharetra magna ac. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum. Molestie ac feugiat sed lectus vestibulum mattis. Sit amet facilisis magna etiam tempor orci eu lobortis. Libero justo laoreet sit amet cursus sit amet dictum sit. At auctor urna nunc id cursus metus aliquam eleifend. Nec ultrices dui sapien eget mi proin sed libero. Tempor id eu nisl nunc mi ipsum faucibus vitae. Egestas dui id ornare arcu odio ut. Sit amet aliquam id diam maecenas ultricies.</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Sapien eget mi proin sed libero enim sed faucibus.
+                    Nisi est sit amet facilisis magna etiam tempor orci eu.
+                    Pharetra convallis posuere morbi leo urna. Id nibh tortor id
+                    aliquet lectus. Turpis egestas pretium aenean pharetra magna
+                    ac. Aliquet lectus proin nibh nisl condimentum id venenatis
+                    a condimentum. Molestie ac feugiat sed lectus vestibulum
+                    mattis. Sit amet facilisis magna etiam tempor orci eu
+                    lobortis. Libero justo laoreet sit amet cursus sit amet
+                    dictum sit. At auctor urna nunc id cursus metus aliquam
+                    eleifend. Nec ultrices dui sapien eget mi proin sed libero.
+                    Tempor id eu nisl nunc mi ipsum faucibus vitae. Egestas dui
+                    id ornare arcu odio ut. Sit amet aliquam id diam maecenas
+                    ultricies.
+                </p>
             </div>
         );
     }
