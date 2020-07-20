0021f283c45c110b0289f7911f3a45c7fdeff7b0
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-07-19 20:06:48 -0500

    DANCING PENGUIN

--- app/ui/containers/panels/MainContentsPanelContainer.tsx
+++ app/ui/containers/panels/MainContentsPanelContainer.tsx
@@ -1,32 +1,88 @@
+import { motion, Variants } from 'framer-motion';
 import React from 'react';
 import mainConfig from '../../configs/impl/MainConfigImpl';
+import './test.global.css';
 
-export default class MainContentsPanelContainer extends React.Component<
-    {},
-    {}
-> {
-    render() {
-        return (
-            <div className={mainConfig.cssClassStyles}>
-                <h1>Main Contents Panel</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Sapien eget mi proin sed libero enim sed faucibus.
-                    Nisi est sit amet facilisis magna etiam tempor orci eu.
-                    Pharetra convallis posuere morbi leo urna. Id nibh tortor id
-                    aliquet lectus. Turpis egestas pretium aenean pharetra magna
-                    ac. Aliquet lectus proin nibh nisl condimentum id venenatis
-                    a condimentum. Molestie ac feugiat sed lectus vestibulum
-                    mattis. Sit amet facilisis magna etiam tempor orci eu
-                    lobortis. Libero justo laoreet sit amet cursus sit amet
-                    dictum sit. At auctor urna nunc id cursus metus aliquam
-                    eleifend. Nec ultrices dui sapien eget mi proin sed libero.
-                    Tempor id eu nisl nunc mi ipsum faucibus vitae. Egestas dui
-                    id ornare arcu odio ut. Sit amet aliquam id diam maecenas
-                    ultricies.
-                </p>
-            </div>
-        );
-    }
-}
+// export default class MainContentsPanelContainer extends React.Component<
+//     {},
+//     {}
+// > {
+//     render() {
+//         return (
+//             <div className={mainConfig.cssClassStyles}>
+//                 <h1>Main Contents Panel</h1>
+//                 <motion.div
+//                     className="rectangle"
+//                     initial={{
+//                         opacity: 0,
+//                         y: 50,
+//                     }}
+//                     animate={{
+//                         opacity: 1,
+//                         y: 0,
+//                     }}
+//                 />
+//             </div>
+//         );
+//     }
+// }
+
+const MainContentsPanelContainer = () => {
+    const [active, setActive] = React.useState(false);
+
+    const container: Variants = {
+        active: {
+            background: '#ff00b1',
+        },
+        disabled: {
+            background: '#0D00FF',
+        },
+    };
+
+    const box: Variants = {
+        active: {
+            rotate: 90,
+            opacity: 1,
+        },
+        disabled: {
+            rotate: 0,
+            opacity: 0.7,
+        },
+    };
+
+    return (
+        <div className={mainConfig.cssClassStyles}>
+            <motion.div
+                className="rectangle"
+                animate={{ rotate: 0 }}
+                onClick={() => setActive(!active)}
+                transition={{
+                    loop: Infinity,
+                    ease: 'linear',
+                    duration: 2,
+                }}
+            >
+                {/* <img src="D:\Projects\tnyPlayer\app\ui\containers\panels\arctic.jpg"/> */}
+                    <motion.img
+                        src="D:\Projects\tnyPlayer\app\ui\containers\panels\peng-jumping.jpg"
+                        width="200"
+                        className="peng"
+                        initial={{
+                            y: 280,
+                        }}
+                        animate={{
+                            rotate: 360,
+                            y: 100,
+                        }}
+                        transition={{
+                            duration: 2,
+                            loop: Infinity,
+                            ease: 'linear',
+                        }}
+                    />
+            </motion.div>
+        </div>
+    );
+};
+
+export default MainContentsPanelContainer;
