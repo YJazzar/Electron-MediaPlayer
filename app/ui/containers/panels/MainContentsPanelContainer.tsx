cd8fe4e2493a901e1777b4baa0365caaa92d3232
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-09-02 23:33:28 -0500

    Added the start of a table in mainPanel

--- app/ui/containers/panels/MainContentsPanelContainer.tsx
+++ app/ui/containers/panels/MainContentsPanelContainer.tsx
@@ -1,6 +1,22 @@
-import { motion } from 'framer-motion';
 import React from 'react';
 import mainConfig from '../../configs/MainConfigImpl';
+import styled from 'styled-components';
+
+// const Table = styled.table`
+// `;
+
+// const TBody = styled.tbody`
+// `;
+
+// const THead = styled.thead`
+// `;
+
+// const TRow = styled.tr`
+// `;
+
+// const TData = styled.td`
+// `;
+
 export default class MainContentsPanelContainer extends React.Component<
     {},
     {}
@@ -9,18 +25,31 @@ export default class MainContentsPanelContainer extends React.Component<
         return (
             <div className={mainConfig.cssClassStyles}>
                 <h1>Main Contents Panel</h1>
-                <motion.div
-                    className="rectangle"
-                    initial={{
-                        opacity: 0,
-                        y: 50,
-                    }}
-                    animate={{
-                        opacity: 1,
-                        y: 0,
-                    }}
-                />
+                {this.getTable()}
             </div>
+        );
+    }
+
+    getTable(): React.ReactChild {
+        const tableHeaders = [
+            'Name',
+            'Length',
+            'Type',
+            'Size',
+            'Date Modified',
+        ];
+
+        const th = [];
+        for (let i = 0; i < tableHeaders.length; i++) {
+            th.push(<td>{tableHeaders[i]}</td>);
+        }
+
+        return (
+            <table className={".w-9/12"}>
+                <thead>
+                    <tr>{th}</tr>
+                </thead>
+            </table>
         );
     }
 }
