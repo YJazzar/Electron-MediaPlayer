18d2bbc1c40258680685575eb14bbe3fe6c30ac6
Basel <basel@Basels-MacBook-Pro-2.local>
2020-12-15 16:45:40 -0600

    Fixed a Mac bug where ffprobe cannot execute

--- app/core/fileOperations/helpers/ffprobeGetDuration.ts
+++ app/core/fileOperations/helpers/ffprobeGetDuration.ts
@@ -1,8 +1,11 @@
 import { exec } from 'child_process';
 import { app } from 'electron';
 import path from 'path';
+import LoggerFactory from '../../../libs/logger/LoggerFactory';
 
 const appRootDir = require('app-root-dir');
+
+const log = LoggerFactory.getLogger(__filename);
 
 function getPlatform(): string {
     switch (process.platform) {
@@ -33,13 +36,12 @@ const cmd = `${path.join(execPath, process.platform === 'win32' ? 'ffprobe.exe' 
 export default function ffprobeGetDuration(filePath: string): Promise<number> {
     const params = [];
     params.push('-v', 'error', '-show_entries', 'format=duration', '-of default=noprint_wrappers=1:nokey=1', `"${filePath}"`);
-
     // The command to run is constructed:
     const command = `${cmd} ${params.join(' ')}`;
-    // console.log(`Running commanfd: ${command}`);
+    console.log(`Running command: ${command}`);
 
     return new Promise((resolve, reject) => {
-        exec(command, (error, stdout) => {
+        exec(command, (error, stdout, stderr) => {
             if (error) {
                 return reject(error);
             }
